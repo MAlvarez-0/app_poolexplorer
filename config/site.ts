0c3ca25ae059b6b5593ad82823bdc3443af5982e
MAlvarez <31598428+MAlvarez-0@users.noreply.github.com>
2023-02-14 23:54:51 +0100

    Initial commit

---
+++ config/site.ts
@@ -0,0 +1,30 @@
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// Site
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+interface SiteConfig {
+  name: string
+  title: string
+  emoji: string
+  description: string
+  previewImg: string
+  localeDefault: string
+  links: {
+    twitter: string
+    github: string
+  }
+}
+
+export const SITE_CANONICAL = 'https://turboeth.xyz'
+
+export const siteConfig: SiteConfig = {
+  name: 'TurboETH',
+  title: 'TurboETH - Web3 App Template',
+  emoji: '⚡',
+  description: 'Web3 App Template built using Next.js, RainbowKit, SIWE, and more!',
+  previewImg: `${SITE_CANONICAL}/preview.png`,
+  localeDefault: 'en',
+  links: {
+    twitter: 'https://twitter.com/KamesGeraghty',
+    github: 'https://github.com/turbo-eth',
+  },
+}
